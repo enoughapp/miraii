a4f5cb852f4f60f9d859adf6d56949f66767bbf9
panitarn <panitarn.work@gmail.com>
2021-09-11 13:30:26 +0700

    first commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Miraii.Models;
+
+namespace Miraii.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Route("admin")]
+        public IActionResult Index()
+        {
+            //admin landing
+            return View();
+        }
+    }
+}
---
+++ Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Miraii.Models;
+
+namespace Miraii.Controllers
+{
+    public class BlogController : Controller
+    {
+        [Route("blog")]
+        public IActionResult Index()
+        {
+            //get all blogs
+            return View();
+        }
+
+        [Route("blog/{id}")]
+        public IActionResult Blog(int id)
+        {
+            //get the selected blog
+            return View();
+        }
+    }
+}
